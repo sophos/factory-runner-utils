614d0ab1d6d82a77613d35645ac6e4aa225fc083
Rick Culpepper <rick.culpepper@sophos.com>
2022-02-14 07:58:04 -0700

    For CentOS Linux 8, convert to Stream 8

--- scripts/install-refactr-agent.sh
+++ scripts/install-refactr-agent.sh
@@ -48,15 +48,20 @@ SYSTEMD
 }
 
 function setup_centos8 {
+    # Is this CentOS Linux 8?
+    [ -f /etc/os-release ] && . /etc/os-release \
+        && [ "$ID" = 'centos' ] \
+        && [ "$VERSION_ID" = '8' ] \
+        && [ "$PRETTY_NAME" == 'CentOS Linux 8' ] \
+        && sed -i -e "s|^mirrorlist=|#mirrorlist=|g" -e "s|^#baseurl=|baseurl=|g" -e "s|^baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-Linux-* \
+        && dnf -y swap centos-linux-repos centos-stream-repos \
+        && dnf -y distro-sync
+
     # Install dnf packages
-    # https://github.com/pyenv/pyenv/wiki
-    #dnf update -y
-    sed -i -e "s|^mirrorlist=|#mirrorlist=|g" -e "s|^#baseurl=|baseurl=|g" -e "s|^baseurl=http://mirror.centos.org|baseurl=http://vault.centos.org|g" /etc/yum.repos.d/CentOS-Linux-* \
-        && dnf upgrade -y
-        && dnf install -y gcc openssh openssh-clients git ca-certificates wget \
-            unzip which jq python3-pip python3-devel @development zlib-devel \
-            bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz \
-            xz-devel libffi-devel findutils glibc-locale-source glibc-langpack-en
+    dnf install -y gcc openssh openssh-clients git ca-certificates wget \
+        unzip which jq python3-pip python3-devel @development zlib-devel \
+        bzip2-devel readline-devel sqlite sqlite-devel openssl-devel xz \
+        xz-devel libffi-devel findutils glibc-locale-source glibc-langpack-en
 
     # Updating locales
     localedef -c -f UTF-8 -i en_US en_US.UTF-8
